085bb97e969563eac6785ca2328bf012cb5c35a8
Shariq Nasir <Shariq.Nasir@ais.com>
2025-09-01 12:07:46 +0530

    upates

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR this is some feature
+          Edit <code>src/App.tsx</code> and save to test HMR this is some feature more changes
         </p>
       </div>
       <p className="read-the-docs">
