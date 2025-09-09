93e081b6110ed82eb1c18e96ddf96e14dc174c30
Shariq Nasir <Shariq.Nasir@ais.com>
2025-09-09 15:23:30 +0530

    test 1

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more... test commit second. this sample  
+        Click on the Vite and React logos to learn more... test commit second. this sample.
       </p>
     </>
   )
